0a04c38e1144020ba6800ac5366983b9bf451575
raythrp <rayhanaprasnaputra@gmail.com>
2024-11-06 12:11:46 +0700

    fixes bug on server dev n prod ports

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ const routes = require('./routes.js');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.NODE_ENV !== 'production' ? 8080 : 5000,
+    host: process.env.NODE_ENV !== 'production' ? '127.0.0.1' : '35.219.117.120',
     routes: {
       cors: {
         origin: ['*']
